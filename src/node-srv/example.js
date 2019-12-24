cc2b5e76717a448fb00d203b10355ac0fd3eef6d
Vic <vic@eml.cc>
2019-12-24 13:38:15 -0500

    rel

--- src/node-srv/example.js
+++ src/node-srv/example.js
@@ -1,19 +1,23 @@
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const Serv_1 = require("./lib/Serv");
 let allowedDomains = [];
 allowedDomains.push('one.com');
 allowedDomains.push('two.org');
 const service = new Serv_1.Serv(['*']);
 class Handler1 extends Serv_1.BaseRPCMethodHandler {
+    constructor() {
+        super(2, 1);
+    }
     multiply(params) {
         let a = params.a;
         let b = params.b;
-        return multiply(a, b);
+        return doMultiply(a, b);
     }
 }
-const h1 = new Handler1(1);
+const h1 = new Handler1();
 service.routeRPC('api', h1);
-function multiply(a, b) {
+function doMultiply(a, b) {
     return a * b;
 }
 service.listen(8888);
