b113ee3aadd8e6959a7518d845c4ff2b3033bb7d
Vic <vic@eml.cc>
2020-03-30 14:34:24 -0400

    d

--- CRUD/webApp/api/LoginVM.js
+++ CRUD/webApp/api/LoginVM.js
@@ -1,38 +1,27 @@
-
-import { EventFlux } from 'https://cdn.jsdelivr.net/gh/intuition-dev/mbToolBelt@v8.4.3/eventFlux/EventFlux.js'
-new EventFlux() // also creates global defEventFlux var
-
+import { EventFlux } from 'https://cdn.jsdelivr.net/gh/intuition-dev/mbToolBelt@v8.4.3/eventFlux/EventFlux.js';
+new EventFlux(); // also creates global defEventFlux var
 // req for rpc
-import { HttpRPC } from 'https://cdn.jsdelivr.net/npm/http-rpc@2.4.1/webApp/httpRPC.min.js'
-
-export class  {
-
+import { HttpRPC } from 'https://cdn.jsdelivr.net/npm/http-rpc@2.4.1/webApp/httpRPC.min.js';
+export class LoginVM {
     constructor() {
- 
-        console.log('cons')
-        let THIZ = this
-        
+        console.log('cons');
+        let THIZ = this;
         THIZ.rpc = new HttpRPC('http', 'localhost', 8888);
-        THIZ.fetch('a', 1);
-
-        defEventFlux.register('checkLogin', function(arg) {
-            THIZ.fetch(arg.srch, arg.o)
-        })
-
-    }//
-
-    fetch(srch, o) {
+        defEventFlux.register('checkLogin', this.checkLogin);
+    } //()
+    checkLogin(o) {
+        console.log(o);
+        if (true)
+            return;
         let args = {};
         args['srch'] = srch;
         args['o'] = o;
         console.log('fetch', args);
         this.rpc.invoke('uapi', 'srch', args)
-            .then(function(resp) {
-                console.log('got data')
-                defEventFlux.dispatch('onUData', resp)
-            })
-    }//()
-
-}//class
-
-new UsersVM()
+            .then(function (resp) {
+            console.log('got data');
+            defEventFlux.dispatch('onUData', resp);
+        });
+    } //()
+} //class
+new LoginVM();
